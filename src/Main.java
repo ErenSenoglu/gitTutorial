71237a9edbc53aa1997caa6dfdfbe0f18f0b2e89
ErenSenogl <eren.senoglu@ug.bilkent.edu.tr>
2020-04-06 22:10:17 +0300

    first try

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+        public static void main(String[] args){
+            int num1 = 100;
+            int num2 = 50;
+            System.out.println("Number 1: "+num1);
+            System.out.println("Number 2:"+num2);
+        }
+        public static void printName(String name){
+            System.out.println("Hello "+name);
+        }
+}
