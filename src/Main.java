6132f10a124ef5283ceececaa3cf674227f0c39d
ErenSenogl <eren.senoglu@ug.bilkent.edu.tr>
2020-04-06 22:45:37 +0300

    sum+div+multiply

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,7 @@ public class Main {
 
             System.out.println("Multiplication: "+multiply(num1,num2));
             System.out.println("Summation: "+sum(num1,num2));
-            
+            System.out.println("Division: "+div(num1,num2));
         }
         public static void printName(String name){
             System.out.println("Hello "+name);
@@ -17,6 +17,9 @@ public class Main {
         }
         public static int sum(int num1, int num2){
             return num1+num2;
+        }
+        public static int div(int num1, int num2){
+            return num1/num2;
         }
 
 }
