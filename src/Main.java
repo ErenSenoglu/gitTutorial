7e69b74796118ce8d70bc8a788dcfcd2993a982c
ErenSenogl <eren.senoglu@ug.bilkent.edu.tr>
2020-04-06 22:12:33 +0300

    eren-dev multiplication

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,13 @@ public class Main {
             int num2 = 50;
             System.out.println("Number 1: "+num1);
             System.out.println("Number 2:"+num2);
+
+            System.out.println("Multiplication: "+multiply(num1,num2));
         }
         public static void printName(String name){
             System.out.println("Hello "+name);
+        }
+        public static int multiply(int num1, int num2){
+            return num1*num2;
         }
 }
